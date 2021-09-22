3dd0efbf9d8df8f1a6715c9fb72a8799c71a3869
JiveOff <antoine@jiveoff.fr>
2021-09-22 22:39:32 +0200

    après atelier

---
+++ NuxtJS/components/AchatItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <input type="number" v-model="item.qte" min="0" :max="item.stock" /> /
+    {{ item.stock }}
+    <span>{{ item.name }} ({{ item.prix }}€ l'unité)</span>
+    <span v-if="item.qte >= item.stock"> - Plus de stock</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style></style>
--- NuxtJS/pages/reservation.vue
+++ NuxtJS/pages/reservation.vue
@@ -1,76 +1,78 @@
 <template>
   <div>
     <navbar></navbar>
-    <main >
+    <main>
+      <p>Vous êtes sur la page de réservation d'hotel. Voici votre panier :</p>
 
-        <p>Vous êtes sur la page de réservation d'hotel. Voici votre panier : </p>
-
-        <section v-if="nbSecondes >= 0" >
-            <h1>Vous avez {{nbSecondes}}s pour faire votre achat</h1>
-            <p>Vous allez dormir {{nbJours}} jours ! <button @click="nbJours--">- jours</button> <button @click="nbJours++">+ jours</button></p>
-            <achats :items="this.items" class="ok"></achats>
-        </section>
-        <p v-else class="nope">
-            Fini tu peux plus choisir.
-
+      <section v-if="nbSecondes >= 0">
+        <h1>Vous avez {{ nbSecondes }}s pour faire votre achat</h1>
+        <p>
+          Vous allez dormir {{ nbJours }} jours !
+          <button @click="nbJours--">- jours</button>
+          <button @click="nbJours++">+ jours</button>
         </p>
+        <achats :items="this.items" class="ok"></achats>
+      </section>
+      <p v-else class="nope">Fini tu peux plus choisir.</p>
 
-        <p>Prix total : {{prixPanier}}€</p>
-        <button @click="nbSecondes += 10">Ajouter 10 secondes dans le timeout</button>
+      <p>Prix total : {{ prixPanier }}€</p>
+      <button @click="nbSecondes += 10">
+        Ajouter 10 secondes dans le timeout
+      </button>
     </main>
   </div>
 </template>
 
 <script>
-import achats from '../components/achats.vue';
-import Navbar from '../components/navbar.vue'
+import achats from "../components/achats.vue";
+import Navbar from "../components/navbar.vue";
 export default {
   components: { achats },
 
-    data() {
-        return {
-            nbJours: 1,
-            nbSecondes: 20,
-            items: [
-                {name: "Adulte", prix: 40, qte: 0, stock: 3},
-                {name: "Enfant", prix: 30, qte: 0, stock: 3},
-                {name: "Chien", prix: 10, qte: 0, stock: 1},
-            ]
-        }
+  data() {
+    return {
+      nbJours: 1,
+      nbSecondes: 20,
+      items: [
+        { name: "Adulte", prix: 40, qte: 0, stock: 3 },
+        { name: "Enfant", prix: 30, qte: 0, stock: 3 },
+        { name: "Chien", prix: 10, qte: 0, stock: 1 },
+      ],
+    };
+  },
+  computed: {
+    prixPanier() {
+      let prixTot = 0;
+      for (let item of this.items) {
+        prixTot += item.prix * item.qte;
+      }
+      return prixTot * this.nbJours;
     },
-    computed: {
-        prixPanier() {
-            let prixTot = 0;
-            for (let item of this.items) {
-                prixTot+= item.prix * item.qte;
-            }
-            return prixTot * this.nbJours;
-        }
-
+  },
+  created() {
+    setInterval(this.tictac, 1000);
+  },
+  methods: {
+    tictac() {
+      this.nbSecondes--;
     },
-    created() {
-        setInterval(this.tictac, 1000);
-    },
-    methods: {
-        tictac() {
-            this.nbSecondes--;
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 main {
-    margin: 2em 10%;
-    padding: 2em;
+  margin: 2em 10%;
+  padding: 2em;
 }
 .ok {
-    background: cornsilk;
+  background: cornsilk;
 }
 .nope {
-    background: coral;
+  background: coral;
 }
-.ok, .nope {
-    padding: 1em;
+.ok,
+.nope {
+  padding: 1em;
 }
 </style>
